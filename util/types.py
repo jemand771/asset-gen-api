1db36911a8ae742909bba64e45b8af7a452a3ebf
Willy <jemand771@gmx.net>
2022-03-13 21:59:04 +0100

    fix mutable cache

--- util/types.py
+++ util/types.py
@@ -1,11 +1,27 @@
 import functools
 from enum import Enum, auto
 
+from PIL import Image
+
 
 class MediaType(Enum):
     text = auto()
     image = auto()
     integer = auto()
+
+
+def copy_if_image(arg):
+    if isinstance(arg, Image.Image):
+        return arg.copy()
+    return arg
+
+
+def image_copier(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*[copy_if_image(x) for x in args], **{key: copy_if_image(value) for key, value in kwargs.items()})
+
+    return wrapper
 
 
 class AllHandlerBase:
@@ -14,6 +30,7 @@ class AllHandlerBase:
     def __init__(self):
         if self.allow_cache:
             self.run = functools.cache(self.run)
+        self.run = image_copier(self.run)
 
     def run(self, *args, **kwargs):
         pass
