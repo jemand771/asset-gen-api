1136a6435e0a58f119bd98f7386e231a28f706b1
Willy <jemand771@gmx.net>
2022-05-07 23:23:29 +0200

    add no bitches generator

---
+++ generators/no_bitches.py
@@ -0,0 +1,31 @@
+from generators.image_from_url import ImageFromServerAsset
+from generators.paste import ForegroundPaste
+from generators.text import BetterText
+from util.types import Box, GeneratorChain, MappedParam, MediaType, Runner
+
+
+class NoBitchesGenerator(GeneratorChain):
+    input_params = {
+        "text": MediaType.text
+    }
+    name = "no_bitches"
+    output_type = MediaType.image
+
+    box = Box(x1=20, x2=519, y1=20, y2=120)
+    chain = Runner(
+        ForegroundPaste,
+        canvas=Runner(
+            ImageFromServerAsset,
+            name="megamind_no_bitches.png"
+        ),
+        box=box,
+        image=Runner(
+            BetterText,
+            box=box.whbox,
+            font="impact",
+            fill_color=(255, 255, 255),
+            stroke_color=(0, 0, 0, 255),
+            stroke_width=5,
+            text=MappedParam(name="text")
+        )
+    )
